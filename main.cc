4a24436c293bad016e66fc16317b5a5c1c39546b
ShakaUVM </u/ShakaUVM>
2021-06-25 15:18:01 -0700

    Updated demo code to match Readme.md

--- main.cc
+++ main.cc
@@ -1,13 +1,13 @@
 #include "colors.h"
 
 int main() {
-	//Simple way of using the code - 
+	//Simple way of using the code - use enums:
 	//Will print Hello World in bright red (and all future text will be in bright red)
 	std::cout << RED << "Hello World!\n"; 
 	//Will print Hello in a bold blue color, World in a bold yellow, and then reset the text colors back to normal for future text
-	std::cout << BOLDBLUE << "Hello " << BOLDYELLOW << " World!" << RESET << std::endl; 
+	std::cout << BOLDBLUE << "Hello" << BOLDYELLOW << " World!" << RESET << std::endl; 
 
-	//Medium difficulty way of using the code -
+	//Medium difficulty way of using the code - specify 24-bit colors:
 	setbgcolor(80,80,40); //Set background color to some ugly brown
 	setcolor(180,0,0); //Set foreground color to 75% red
 	std::cout << "Hello World" << RESET << std::endl;
